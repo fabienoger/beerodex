70c57ba1d0587fe46d773ab1ff411253f0075978
fabienoger <fab-oger@live.fr>
2015-11-09 23:21:17 +0100

    Beer controller & viexs finish

--- app/controllers/beer_controller.rb
+++ app/controllers/beer_controller.rb
@@ -1,4 +1,7 @@
 class BeerController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin
+
   def index
     @beers = Beer.all
   end
@@ -8,17 +11,57 @@ class BeerController < ApplicationController
   end
 
   def create
+    @beer = Beer.new(:name => params[:beer][:name], :degrees => params[:beer][:degrees], :country => params[:beer][:country], :quality_hangover => params[:beer][:quality_hangover], :technical_sheet => params[:beer][:technical_sheet])
+    if @beer.save
+      flash[:success] = "La bière à bien été enregistré."
+    else
+      flash[:error] = "La bière n'a pas été enregistré."
+    end
+    redirect_to '/admin/beers'
   end
 
   def show
+    @beer = Beer.find(params[:id])
   end
 
   def edit
+    @beer = Beer.find(params[:id])
   end
 
   def update
+    @beer = Beer.find(params[:id])
+    @beer.name = params[:beer][:name]
+    @beer.degrees = params[:beer][:degrees]
+    @beer.country = params[:beer][:country]
+    @beer.quality_hangover = params[:beer][:quality_hangover]
+    @beer.technical_sheet = params[:beer][:technical_sheet]
+    if @beer.save
+      flash[:success] = "La bière à bien été modifié."
+      redirect_to '/admin/beers'
+    else
+      flash[:error] = "La bière n'a pas été modifié."
+      redirect_to request.referer || '/admin/beers'
+    end
   end
 
   def destroy
+    beer = Beer.find(params[:id])
+    beer.destroy
+    if beer.save
+      flash[:success] = "La bière à bien été supprimé."
+    else
+      flash[:error] = "La bière n'a pas été supprimé."
+    end
+    redirect_to '/admin/beers'
+  end
+
+  private
+
+  def check_admin
+    if current_user.admin?
+      return true
+    else
+      redirect_to '/'
+    end
   end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,17 @@
 class Beer < ActiveRecord::Base
+  validates :name, presence: {
+    message: "Le nom de la bière doit être renseigné."
+  }
+  validates :degrees, presence: {
+    message: "Le degrès doit être renseigné."
+  }
+  validates :country, presence: {
+    message: "Le pays doit être renseigné."
+  }
+  validates :quality_hangover, presence: {
+    message: "Le rapport qualité cuite de la bière doit être renseigné."
+  }
+  validates :technical_sheet, presence: {
+    message: "La fiche technique doit être renseigné."
+  }
 end
